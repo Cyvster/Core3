1221563d3be7a9fc9678a925192214f7a2130614
Cyvster <41338761+Cyvster@users.noreply.github.com>
2024-01-12 18:50:15 -0600

    Update city_manager.lua
    set city maintenance to 0%

--- MMOCoreORB/bin/scripts/managers/city_manager.lua
+++ MMOCoreORB/bin/scripts/managers/city_manager.lua
@@ -85,7 +85,7 @@ TrainersPerRank = 15
 MissionTerminalsPerRank = 10
 
 -- Amount to discount city maintenance  float.  1 = 100%, .75 =75%, .5=50% etc
-maintenanceDiscount = .05
+maintenanceDiscount = 0.0
 
 --[[
 	CITIES ALLOWED PER PLANET
