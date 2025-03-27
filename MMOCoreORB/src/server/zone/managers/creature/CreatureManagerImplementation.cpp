884ffdcf331059d3ab278899dde795128d23215a
Cyvster <41338761+Cyvster@users.noreply.github.com>
2025-03-27 09:55:12 -0500

    Update CreatureManagerImplementation.cpp
    compile error, remove unnecessary return

--- MMOCoreORB/src/server/zone/managers/creature/CreatureManagerImplementation.cpp
+++ MMOCoreORB/src/server/zone/managers/creature/CreatureManagerImplementation.cpp
@@ -696,7 +696,6 @@ int CreatureManagerImplementation::notifyDestruction(TangibleObject* destructor,
 				ManagedReference<CreatureObject*> highestDamagePlayer = copyThreatMap.getHighestDamagePlayer(); // Get the player who did the most damage
 				if (highestDamagePlayer && highestDamagePlayer->isPlayerCreature()) {
 				        highestDamagePlayer->addCashCredits(credits); // Award credits to the highest damage player
-				        return;
 				}
 				destructedObject->addCashCredits(credits); // Fallback if no valid player is found
 
