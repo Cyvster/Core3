cacc87cd0eff32249a3552b211a0d1c59d62a731
Cyvster <41338761+Cyvster@users.noreply.github.com>
2025-03-31 17:27:32 -0500

    Update armor_mandalorian_shoes.lua

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armor/mandalorian/armor_mandalorian_shoes.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armor/mandalorian/armor_mandalorian_shoes.lua
@@ -99,7 +99,7 @@ object_tangible_wearables_armor_mandalorian_armor_mandalorian_shoes = object_tan
 	mindEncumbrance = 0,
 
 	-- LIGHT, MEDIUM, HEAVY
-	rating = LIGHT,
+	rating = MEDIUM,
 	maxCondition = 250000,
 
 	kinetic = 80,
