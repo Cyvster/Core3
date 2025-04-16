354cdf990b2835c581c94351cdb7e43b8e062208
Cyvster <41338761+Cyvster@users.noreply.github.com>
2025-04-15 19:07:02 -0500

    Update tatooine_hard_krayt_ne.lua
    spawn - increased krayt spawns in northeast tatooine

--- MMOCoreORB/bin/scripts/mobile/spawn/tatooine/tatooine_hard_krayt_ne.lua
+++ MMOCoreORB/bin/scripts/mobile/spawn/tatooine/tatooine_hard_krayt_ne.lua
@@ -2,7 +2,7 @@ tatooine_hard_krayt_ne = {
 	lairSpawns = {
 		{
 			lairTemplateName = "tatooine_canyon_krayt_dragon_pack_neutral_none",
-			spawnLimit = 4,
+			spawnLimit = -1,
 			minDifficulty = 122,
 			maxDifficulty = 276,
 			numberToSpawn = 0,
@@ -11,29 +11,29 @@ tatooine_hard_krayt_ne = {
 		},
 		{
 			lairTemplateName = "tatooine_giant_canyon_krayt_dragon_pack_neutral_none",
-			spawnLimit = 4,
+			spawnLimit = -1,
 			minDifficulty = 275,
 			maxDifficulty = 304,
 			numberToSpawn = 0,
-			weighting = 30,
+			weighting = 50,
 			size = 20
 		},
 		{
 			lairTemplateName = "tatooine_krayt_dragon_grand_neutral_none",
-			spawnLimit = 2,
+			spawnLimit = -1,
 			minDifficulty = 320,
 			maxDifficulty = 350,
 			numberToSpawn = 0,
-			weighting = 15,
+			weighting = 50,
 			size = 20,
 		},
 		{
 			lairTemplateName = "tatooine_krayt_dragon_ancient_neutral_none",
-			spawnLimit = 2,
+			spawnLimit = -1,
 			minDifficulty = 320,
 			maxDifficulty = 350,
 			numberToSpawn = 0,
-			weighting = 15,
+			weighting = 50,
 			size = 20,
 		},
 	}
