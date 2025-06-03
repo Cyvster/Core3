6bd58b48e8c06c4d13f1c71d46411617df0ee5f2
Cyvster <41338761+Cyvster@users.noreply.github.com>
2025-06-03 11:48:30 -0500

    Update deathWatchBunker.lua
    armor - set mandalorian armor to have 6 sockets

--- MMOCoreORB/bin/scripts/screenplays/dungeon/death_watch_bunker/deathWatchBunker.lua
+++ MMOCoreORB/bin/scripts/screenplays/dungeon/death_watch_bunker/deathWatchBunker.lua
@@ -139,6 +139,9 @@ DeathWatchBunkerScreenPlay = ScreenPlay:new {
 
 	primaryArmorColors = { 48, 48, 48, 48, 48, 48, 48, 48 },--{ 48, 55, 60, 95, 111, 127, 135, 143 },
 	secondaryArmorColors = { 42, 90, 236, 244, 252, 253, 254, 255 }
+	--set socket count on mando armor, thanks Bagsie
+	TangibleObject(pReward):setSocketCount(6);
+	--set socket count on mando armor, thanks Bagsie
 }
 
 registerScreenPlay("DeathWatchBunkerScreenPlay", true)
