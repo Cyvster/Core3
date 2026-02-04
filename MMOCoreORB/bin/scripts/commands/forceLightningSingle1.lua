641d19297e44f421a8b4da4fe0dbfbbe9ed62654
Cyvster <41338761+Cyvster@users.noreply.github.com>
2026-02-04 05:46:43 -0600

    Increase damage modifiers for lightning attack

--- MMOCoreORB/bin/scripts/commands/forceLightningSingle1.lua
+++ MMOCoreORB/bin/scripts/commands/forceLightningSingle1.lua
@@ -62,10 +62,10 @@ ForceLightningSingle1Command = {
 	forceAttack = true,
 	damageType = ELECTRICITY_DAMAGE,
 
-	frsLightMinDamageModifier = 2.5,
-	frsLightMaxDamageModifier = 5,
-	frsDarkMinDamageModifier = 2.5,
-	frsDarkMaxDamageModifier = 5,
+	frsLightMinDamageModifier = 25,
+	frsLightMaxDamageModifier = 50,
+	frsDarkMinDamageModifier = 25,
+	frsDarkMaxDamageModifier = 50,
 
 	range = 32
 }
