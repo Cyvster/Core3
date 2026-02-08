3d190595a95eb63a3f47806c4b1b25635e429068
Cyvster <41338761+Cyvster@users.noreply.github.com>
2026-02-08 10:04:23 -0600

    undo previous changes

--- MMOCoreORB/src/server/zone/objects/creature/buffs/PlayerVehicleBuffImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/creature/buffs/PlayerVehicleBuffImplementation.cpp
@@ -117,7 +117,7 @@ void PlayerVehicleBuffImplementation::updateRiderSpeeds() {
 			}
 
 			// Send riders speed update
-			rider->updateRunSpeed() * 2.0f; //attempt to double riding speed
+			rider->updateRunSpeed();
 		}
 	}, "UpdateRiderSpeedsLambda");
 }
