bdc22ae283b2128a7ec5bfae1e52f17dab0362bb
Cyvster <41338761+Cyvster@users.noreply.github.com>
2026-02-09 07:47:58 -0600

    undo previous speed changes that did not work

--- MMOCoreORB/src/server/zone/objects/creature/buffs/PlayerVehicleBuffImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/creature/buffs/PlayerVehicleBuffImplementation.cpp
@@ -89,7 +89,6 @@ void PlayerVehicleBuffImplementation::updateRiderSpeeds() {
 
 		// add speed multiplier mod
 		newSpeed *= vehicle->getSpeedMultiplierMod();
-		newSpeed *= 2.0f;//attempt to double riding speed
 
 		// Update Vehicles Speed
 		vehicle->setRunSpeed(newSpeed);
