df23e088402c9ca9ee83fdd800274c6f7fb2d707
Cyvster <41338761+Cyvster@users.noreply.github.com>
2026-08-19 09:24:21 -0500

    revert

--- MMOCoreORB/src/server/zone/managers/objectcontroller/command/CommandConfigManager.cpp
+++ MMOCoreORB/src/server/zone/managers/objectcontroller/command/CommandConfigManager.cpp
@@ -341,6 +341,9 @@ void CommandConfigManager::registerSpecialCommands(CommandList* sCommands) {
 
 	slashCommands = sCommands;
 
+	QueueCommand* customSkills = new CustomSkillCommand("customskills", server);
+	slashCommands->put(customSkills);
+
 	QueueCommand* admin = new AdminCommand("admin", server);
 	slashCommands->put(admin);
 
